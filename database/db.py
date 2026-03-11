7af0e264926c3531a66949e1d375584bd36b1856
NiniLaNouille <fanny.lebeguec@gmail.com>
2026-03-11 17:33:36 +0200

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,6 +1,10 @@
 import sqlite3
+import os
 from config import DB_PATH
 
-
 def get_connection():
+
+    # ensure folder exists
+    os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+
     return sqlite3.connect(DB_PATH)
