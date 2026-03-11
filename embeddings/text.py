51a9da36e32494e804ca723750d053966b9001be
NiniLaNouille <fanny.lebeguec@gmail.com>
2026-03-11 17:20:12 +0200

    Add text embedding functionality using SentenceTransformer

---
+++ embeddings/text.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+
+def embed(text):
+
+    vector = model.encode(text)
+
+    return vector
