c93118f829766c6a78586f9e2ba5715a92258059
NiniLaNouille <fanny.lebeguec@gmail.com>
2026-03-11 17:19:45 +0200

    Create zalando.py

---
+++ scrapers/zalando.py
@@ -0,0 +1,18 @@
+def scrape():
+
+    products = []
+
+    products.append({
+        "id": "zalando_test",
+        "title": "Black Wool Coat",
+        "brand": "Mango",
+        "category": "coat",
+        "color": "black",
+        "price": 129,
+        "description": "Oversized wool coat",
+        "image_url": "",
+        "product_url": "",
+        "source": "zalando"
+    })
+
+    return products
