100dce0e288da49621f2c8596d8c5a412e686efb
NiniLaNouille <fanny.lebeguec@gmail.com>
2026-03-20 10:20:59 +0200

    Integrate Zalando scraper into home endpoint

--- api/server.py
+++ api/server.py
@@ -1,16 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from scrapers.zalando import scrape
 
 app = FastAPI()
-
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
 
-    return """
+    products = scrape()[:10]
 
-    <h1>Fashion Recommender</h1>
+    html = "<h1>Weekly Fashion Picks</h1>"
 
-    <p>Your weekly recommendations will appear here.</p>
+    for p in products:
 
-    """
+        html += f"""
+        <div style='margin-bottom:30px'>
+            <img src='{p["image_url"]}' width='200'/>
+            <p><b>{p["brand"]}</b></p>
+            <p>{p["title"]}</p>
+            <p>{p["price"]}€</p>
+        </div>
+        """
+
+    return html
